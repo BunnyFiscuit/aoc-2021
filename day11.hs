55aaaa5563e6957e820ed2d6495f088fe39ad651
Sarosh Nasir <sarosh.nasir@devies.se>
2021-12-13 18:04:25 +0100

    start on day 12

--- day11.hs
+++ day11.hs
@@ -10,9 +10,19 @@ main n = do
   ls <- readF 11
   let ms = map (map digitToInt) ls
   eval n ms
+  
+main2 :: IO ()
+main2 = do
+  ls <- readF 11
+  let ms = map (map digitToInt) ls
+  eval2 ms
 
 type Matrix = [[Int]]
 type Pos = (Int, Int)
+
+showM :: Matrix -> IO ()
+showM mx = do
+  putStrLn $ str (toStrArr mx)
 
 example :: Matrix
 example = [
@@ -22,6 +32,12 @@ example = [
   [1,9,9,9,1],
   [1,1,1,1,1]
   ]
+
+ex2 :: Matrix
+ex2 = [[5,4,8,3,1,4,3,2,2,3],[2,7,4,5,8,5,4,7,1,1],[5,2,6,4,5,5,6,1,7,3],[6,1,4,1,3,3,6,1,4,6],[6,3,5,7,3,8,5,4,7,8],[4,1,6,7,5,2,4,6,4,5],[2,1,7,6,8,4,1,7,2,1],[6,8,8,2,8,8,1,1,3,4],[4,8,4,6,8,4,8,5,5,4],[5,2,8,3,7,5,1,5,2,6]]
+
+tex2 :: Matrix
+tex2 = [[7,6,10,5,3,6,5,4,4,5],[4,9,6,7,10,7,6,9,3,3],[7,4,8,6,7,7,8,3,9,5],[8,3,6,3,5,5,8,3,6,8],[8,5,7,9,5,10,7,6,9,10],[6,3,8,9,7,4,6,8,6,7],[4,3,9,8,10,6,3,9,4,3],[8,10,10,4,10,10,3,3,5,6],[6,10,6,8,10,6,10,7,7,6],[7,4,10,5,9,7,3,7,4,8]]
 
 step1 :: Matrix
 step1 = [[2,2,2,2,2],[2,10,10,10,2],[2,10,2,10,2],[2,10,10,10,2],[2,2,2,2,2]]
@@ -35,18 +51,37 @@ eval n mx = do
   putStrLn $ str (toStrArr res)
   putStrLn $ "Total of " ++ show v ++ " flashes"
 
+eval2 :: Matrix -> IO ()
+eval2 mx = do
+  let (res, v) = execState allFlash (mx,0)
+  putStrLn $ "All flash after " ++ show v ++ " steps!"
+  putStrLn $ str (toStrArr res)
+  
+type StateM2 a = State Matrix a
+
+allFlash :: StateM Int
+allFlash = do
+  (mx,step) <- get
+  let octos = length (head mx) * length mx
+  let ff = length $ (filter (==0) (concat mx))
+  case ff == octos of
+    True  -> return step
+    False -> do
+      let mx' = tick mx
+      put (mx', step+1)
+      ripple'
+      allFlash 
+
 type StateM a = State (Matrix,Int) a
 startState = (example, 0)
 
 run :: Int -> StateM Int
 run 0 = do
   (mx, v) <- get
-  -- traceM (str (toStrArr mx))
   return v
 
 run n = do
   (mx, v) <- get
-  traceM (str (toStrArr mx))
   put (tick mx, v)
   ripple
   run (n-1)
@@ -58,19 +93,40 @@ str (x:xs) = x ++ "\n" ++ str xs
 tick :: Matrix -> Matrix
 tick mx = map (map (+1)) mx
 
+ripple' :: StateM Matrix
+ripple' = do
+  (mx, v) <- get
+  let end = (length (head mx), length mx)
+  let ps = findFlashes mx
+  let adjs = fInvalid end (concat (map adjPos ps))
+  put (reset mx, v)
+  case adjs of 
+    [] -> do
+      (mx, v) <- get
+      let mx' = reset mx
+      return mx'
+    _  -> do
+      ip <- incPos adjs
+      put (ip, v)
+      ripple'
+  
+
 ripple :: StateM (Matrix, Int)
 ripple = do
   (mx, v) <- get
   let end = (length (head mx), length mx)
-  -- traceShowM end
   let ps = findFlashes mx
   let adjs = fInvalid end (concat (map adjPos ps))
+  put (reset mx, v)
   case adjs of 
-    [] -> get
+    [] -> do
+      (mx, v) <- get
+      let mx' = reset mx
+      put (mx', v + count mx')
+      get
     _  -> do
-      mx' <- incPos adjs
-      let rs = reset mx'
-      put (rs, v + count rs)
+      ip <- incPos adjs
+      put (ip, v)
       ripple
 
 incPos :: [Pos] -> StateM Matrix
@@ -78,9 +134,8 @@ incPos [] = get >>= \(mx, _) -> return mx
 incPos ((x,y):ps) = do
   (mx, v) <- get
   let val = (mx !! y) !! x
-  -- traceShowM val
-  let mx' = insertM (x,y) (val+1) mx
-  -- traceM (str (toStrArr (reset mx'))) 
+  let val' = if val == 0 then 0 else val+1
+  let mx' = insertM (x,y) val' mx
   put (mx', v)
   incPos ps
 
---
+++ day12.hs
@@ -0,0 +1,65 @@
+module Day12 where
+import System.IO
+import FileReader
+import Data.Char
+import Data.List
+import Control.Monad.State
+
+main :: IO ()
+main = do
+  contents <- readF 12
+  -- putStrLn $ show contents
+  let ps = parseStr contents
+  putStrLn $ show ps
+  -- mapM_ (putStrLn . show) ps
+
+
+data MRecord = MRecord { 
+  edges :: Edges,
+  paths :: [Path]  
+} deriving (Show)
+
+type EState a = State MRecord a
+
+data Node  = Start | End | B String | S String deriving (Show, Eq)
+type Edge  = (Node, Node) 
+type MultiEdge = (Node, [Node])
+type Edges = [Edge]
+type Path  = [Node]
+
+
+parseStr :: [String] -> [(Node, Node)]
+parseStr [] = []
+parseStr (x:xs) = (str2Node start, str2Node end) : parseStr xs
+  where start = takeWhile (/='-') x
+        end   = drop 1 (dropWhile(/='-') x)
+
+str2Node :: String -> Node
+str2Node "start" = Start
+str2Node "end"   = End
+str2Node x       = toCave x
+
+toCave :: String -> Node
+toCave x = if isBigCave x then B x else S x
+
+isBigCave :: String -> Bool
+isBigCave xs = and (map isUpper xs)
+
+correctEdges :: Edges -> Edges
+correctEdges [] = []
+correctEdges ((e,Start):es) = (Start,e) : correctEdges es
+correctEdges ((End,e)  :es) = (e,End) : correctEdges es
+correctEdges (e:es)             = e : correctEdges es 
+
+startEdges :: Edges -> Edges
+startEdges es = filter isStartEdge es
+
+isStartEdge :: Edge -> Bool
+isStartEdge (Start,_) = True
+isStartEdge _         = False
+
+groupEdges :: Edges -> [Edges]
+groupEdges = groupBy (\x y -> fst x == fst y)
+
+makeMultiEdges :: [Edges] -> [MultiEdge]
+makeMultiEdges = undefined
